cadea2c65f55c72f9aa5ed931da183350f473f55
matheusdiass1 <matheus_oliveiradias@hotmail.com>
2025-04-08 08:27:50 -0300

    Alteração cor do texto com JS

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 let body = document.querySelector("body")
 let tenis = document.querySelector(".imagem-tenis")
+let valor = document.querySelector(".texto-real")
 
 function mudarVisual(cor, imagem) {
     tenis.classList.add("troca-efeito")
     body.style.background = cor
+    valor.style.color = cor
 
     setTimeout(() => {
         tenis.src = imagem
