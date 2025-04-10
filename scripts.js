5ff35768ace4e652fae6a5dd830582101ecfb672
matheusdiass1 <matheus_oliveiradias@hotmail.com>
2025-04-09 22:09:18 -0300

    Alteração no botao do carrinho

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 let body = document.querySelector("body")
 let tenis = document.querySelector(".imagem-tenis")
 let valor = document.querySelector(".texto-real")
+const botao = document.querySelector("#carrinho-botao")
 
 function mudarVisual(cor, imagem) {
     tenis.classList.add("troca-efeito")
     body.style.background = cor
     valor.style.color = cor
+    botao.style.background = cor
 
     setTimeout(() => {
         tenis.src = imagem
